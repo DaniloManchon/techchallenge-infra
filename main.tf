8ec1bce40aa4b48ee2bd93ca163a3a787ce79058
Danilo Manchon <danilomanchon@gmail.com>
2024-09-14 12:54:52 -0300

    add files

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.66.0"
+    }
+  }
+
+  required_version = "~> 1.9.5"
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = ""
+  secret_key = ""
+  token = ""
+}
+
