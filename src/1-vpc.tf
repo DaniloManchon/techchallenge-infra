7699dc3522dd485b2105b4272a4451dc55f780c6
Danilo Manchon <danilomanchon@gmail.com>
2024-09-21 15:14:20 -0300

    change vpc link

--- src/1-vpc.tf
+++ src/1-vpc.tf
@@ -42,10 +42,22 @@ resource "aws_security_group" "eks_security_group" {
 
 # }
 
+data "aws_subnets" "private_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+data "aws_subnet" "private_subnets_list" {
+  for_each = toset(data.aws_subnets.example.ids)
+  id       = each.value
+}
+
 resource "aws_apigatewayv2_vpc_link" "techchallenge_vpc_link" {
   name               = "${var.cluster_name}-vpc-link"
   security_group_ids = aws_security_group.eks_security_group.id
-  subnet_ids = data.aws_subnets.techchallenge_vpc.ids
+  subnet_ids = data.aws_subnets.private_subnets_list.ids
   depends_on = [ 
     aws_subnet.techchallenge_private_subnet_1,
     aws_subnet.techchallenge_private_subnet_2
