0086e699c936b24da124c82b0742f723ef0d13e4
Danilo Manchon <danilomanchon@gmail.com>
2024-09-25 20:27:38 -0300

    setting nginx values

--- src/1-vpc.tf
+++ src/1-vpc.tf
@@ -1,8 +1,8 @@
 # VPC
 resource "aws_vpc" "techchallenge_vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     Name = "${var.cluster_name}-vpc"
@@ -39,7 +39,9 @@ resource "aws_security_group" "eks_security_group" {
 data "aws_lb" "techchallenge_ingress_lb" {
   tags = {
     "kubernetes.io/service-name" = "ingress-nginx/ingress-nginx-controller"
-    }
+  }
+
+  depends_on = [helm_release.ingress]
 }
 
 #VPC Link
@@ -47,7 +49,7 @@ resource "aws_api_gateway_vpc_link" "techchallenge_vpc_link" {
   name        = "${var.cluster_name}-vpc-link"
   target_arns = [data.aws_lb.techchallenge_ingress_lb.arn]
 
-  depends_on = [ helm_release.ingress ]
+  depends_on = [helm_release.ingress]
 }
 
 #VPC Endpoint
@@ -55,10 +57,10 @@ resource "aws_vpc_endpoint" "techchallenge_vpc_endpoint" {
   private_dns_enabled = true
   security_group_ids  = [aws_security_group.eks_security_group.id]
   service_name        = "com.amazonaws.${var.aws_region}.execute-api"
-  subnet_ids          = [
+  subnet_ids = [
     aws_subnet.techchallenge_private_subnet_1.id,
     aws_subnet.techchallenge_private_subnet_2.id
   ]
-  vpc_endpoint_type   = "Interface"
-  vpc_id              = aws_vpc.techchallenge_vpc.id
+  vpc_endpoint_type = "Interface"
+  vpc_id            = aws_vpc.techchallenge_vpc.id
 }
