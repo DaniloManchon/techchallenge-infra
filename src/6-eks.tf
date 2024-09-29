a52e3ea9f2a4cbd9b270963297c5aee757a5ad37
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2024-09-29 18:27:23 -0300

    feat: infra

--- src/6-eks.tf
+++ src/6-eks.tf
@@ -1,18 +1,18 @@
-## EKS Cluster
-#resource "aws_eks_cluster" "cluster" {
-#  name     = "${var.cluster_name}-cluster"
-#  role_arn = var.node_role_arn
-#
-#  vpc_config {
-#    subnet_ids = [
-#      aws_subnet.public_subnet_1.id,
-#      aws_subnet.public_subnet_2.id,
-#      aws_subnet.private_subnet_1.id,
-#      aws_subnet.private_subnet_2.id
-#    ]
-#  }
-#
-#  tags = {
-#    Name = "${var.cluster_name}-cluster"
-#  }
-#}
+# EKS Cluster
+resource "aws_eks_cluster" "cluster" {
+  name     = "${var.cluster_name}-cluster"
+  role_arn = var.node_role_arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public_subnet_1.id,
+      aws_subnet.public_subnet_2.id,
+      aws_subnet.private_subnet_1.id,
+      aws_subnet.private_subnet_2.id
+    ]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-cluster"
+  }
+}
--- src/7-nodes.tf
+++ src/7-nodes.tf
@@ -1,31 +1,31 @@
-## EKS Node Group
-#resource "aws_eks_node_group" "node_group" {
-#  cluster_name    = aws_eks_cluster.cluster.name
-#  node_group_name = "${var.cluster_name}-ng"
-#  node_role_arn   = aws_eks_cluster.cluster.role_arn
-#  subnet_ids = [
-#    aws_subnet.private_subnet_1.id,
-#    aws_subnet.private_subnet_2.id
-#  ]
-#
-#  scaling_config {
-#    desired_size = 2
-#    max_size     = 3
-#    min_size     = 1
-#  }
-#
-#  lifecycle {
-#    prevent_destroy = false
-#  }
-#
-#  instance_types = [var.instance_type]
-#  ami_type       = var.ami_type
-#
-#  depends_on = [aws_eks_cluster.cluster]
-#
-#  labels = {}
-#
-#  tags = {
-#    Name = "${var.cluster_name}-ng"
-#  }
-#}
+# EKS Node Group
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "${var.cluster_name}-ng"
+  node_role_arn   = aws_eks_cluster.cluster.role_arn
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  instance_types = [var.instance_type]
+  ami_type       = var.ami_type
+
+  depends_on = [aws_eks_cluster.cluster]
+
+  labels = {}
+
+  tags = {
+    Name = "${var.cluster_name}-ng"
+  }
+}
