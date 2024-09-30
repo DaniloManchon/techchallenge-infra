94621e19ea175e057965b377ac6d1f12145f2ba5
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2024-09-30 17:33:33 -0300

    feat: api gateway http_method

--- src/9-apigateway.tf
+++ src/9-apigateway.tf
@@ -55,7 +55,7 @@ resource "aws_api_gateway_integration" "token_integration" {
   rest_api_id             = aws_api_gateway_rest_api.token_api.id
   resource_id             = aws_api_gateway_resource.token_resource.id
   http_method             = aws_api_gateway_method.token_method.http_method
-  integration_http_method = "POST"
+  integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:975748149223:function:postech-serverless/invocations"
 
@@ -67,7 +67,7 @@ resource "aws_api_gateway_integration" "token_integration_with_cpf" {
   rest_api_id             = aws_api_gateway_rest_api.token_api.id
   resource_id             = aws_api_gateway_resource.token_resource_with_cpf.id
   http_method             = aws_api_gateway_method.token_method_with_cpf.http_method
-  integration_http_method = "POST"
+  integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:975748149223:function:postech-serverless/invocations"
 }
